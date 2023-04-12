fb8739b23a077f0cb5b6d4d7634030b3e2c08ab2
alimacosen <caifanglei1998@gmail.com>
2023-04-12 05:00:07 -0700

    save

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Title
 
+## Description
+https://wiki.debian.org/RepositoryFormat#A.22Contents.22_indices
+http://ftp.uk.debian.org/debian/dists/bullseye/main/
+
+## Installation
+
+
+
+## Usage
+
+
+
+## Sample Output
+
+
+
+## Development Thoughts
+
+
+## Credits and Acknowledgments
+Fanglei Cai  
+caifanglei1998@gmail.com
--- app/procedures/analysis.py
+++ app/procedures/analysis.py
@@ -1,5 +1,5 @@
 from app.config import Config
-from app.components import *
+from app.components import Downloader, Parser
 from app.utils.utils import calc_indent_space
 
 config = Config()
@@ -14,19 +14,23 @@ def get_packages_cnt() -> None:
         Returns:
             None
     """
+    # get configuration: taeget architecture and top k number
     arch = config.get_arch()
     k_statistics = config.get_statistics_num()
 
+    # output format
     indent_space = calc_indent_space()
     package_name_space = config.get_package_name_space()
     files_num_space = config.get_files_num_space()
     index_number = 1
 
+    # main logics of analyzing the contents file.
     downloader = Downloader(config)
     contents = downloader.download_contents_file()
     parser = Parser()
     package_counts = parser.parse_contents_file(contents)
 
+    # show statistics
     print(f"Top {k_statistics} packages with the most files for {arch}:\n")
     print(f"{'':<{indent_space}} {'Package name':<{package_name_space}} {'Num of files':<{files_num_space}}")
     for package, count in package_counts.most_common(k_statistics):
